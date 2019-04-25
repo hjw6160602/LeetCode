f3adb4b4bf53d98368a624fa6a185496d751625e
saidicaprio <hjw6160602@gmail.com>
2019-04-25 21:00:09 +0800

    add code about largest top k

--- LeetCode/347. Top K Frequent Elements/main.swift
+++ LeetCode/347. Top K Frequent Elements/main.swift
@@ -10,6 +10,9 @@ import Foundation
 
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+//        let map = [Int: Int]() {
+//
+//        }
         return []
     }
 }
@@ -17,7 +20,6 @@ class Solution {
 class SolutionSwift {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
         var map = [Int: Int]()
-        
         for num in nums {
             guard let times = map[num] else {
                 map[num] = 1
@@ -35,10 +37,30 @@ class SolutionSwift {
     }
 }
 
-//let s = Solution()
+let s = SolutionSwift()
+let res = s.topKFrequent([3,3,2,2,1,1], 2)
+print(res)
+
 //let heap = Heap.init([7,5,6,4,2,1], capacity: 10)
 //let heap = Heap.init([33,27,21,16,13,15,9,5,6,7,8,1,2], capacity: 20)
 //heap.insert(22)
-let heap = Heap.init([33,27,21,16,13,15,19,5,6,7,8,1,2,12], capacity: 20)
-heap.removeMax()
-print(heap.array)
+
+//let heap = Heap.init([33,27,21,16,13,15,19,5,6,7,8,1,2,12], capacity: 20)
+//heap.removeMax()
+//print(heap.array)
+
+var array = Array.init(repeating: 0, count: 10000)
+for index in 0..<10000 {
+     let num: Int = Int(arc4random() % 100000) + 1
+    array[index] = num
+}
+let heap = Heap.init(10)
+
+for index in 0..<array.count {
+    if array.count > heap.array[0] {
+        
+    }
+    heap.insert(item)
+}
+print(heap.array[10])
+

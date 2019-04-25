73bb9b250c95b056cd841331db12ee6ab65a4ff9
saidicaprio <hjw6160602@gmail.com>
2019-04-25 18:56:17 +0800

    add remove max

--- LeetCode/347. Top K Frequent Elements/Heap.swift
+++ LeetCode/347. Top K Frequent Elements/Heap.swift
@@ -10,30 +10,60 @@ import Foundation
 
 class Heap {
     // 数组，从下标 1 开始存储数据
-    private var a: [Int]
+    var array: [Int]
     // 堆可以存储的最大数据个数
-    private var n: Int
+    private var max: Int
     // 堆中已经存储的数据个数
     private var count: Int
     
     init(_ capacity: Int) {
-        a = Array.init(repeating: 0, count: capacity)
-        n = capacity
+        array = Array.init(repeating: 0, count: capacity)
+        max = capacity
         count = 0
     }
     
+    init(_ array: [Int], capacity: Int) {
+        self.array = array
+        max = capacity
+        count = array.count
+    }
+    
     func insert(_ item: Int) {
-        guard n < count else {// 堆满了
+        guard count < max else {// 堆满了
             return
         }
-        count += 1
-        a[count] = item
+        array.append(item)
         var i = count
-        // 自下往上堆化
-        while i/2 > 0 && a[i] > a[i/2] {
+        var parent = (i - 1) / 2
+        // 自下往上堆化 如果父节点小于插入节点那么交换位置
+        while parent >= 0 && array[i] > array[parent] {
             //交换下标为 i 和 i/2 的两个元素
-            (a[i], a[i/2]) = (a[i/2], a[i])
-            i /= 2
+            (array[i], array[parent]) = (array[parent], array[i])
+            i = parent
+            parent = (i - 1) / 2
+        }
+    }
+    
+    func removeMax() {
+        guard count > 0 else {
+            return
+        }
+        array[0] = array.last!
+        count -= 1
+        array.removeLast()
+        var i = 0
+        while true {
+            var k = i
+            if ((i * 2 + 1) < count && array[i] < array[i * 2 + 1]) {
+                k = i * 2 + 1
+            } else if ((i * 2 + 2) < count && array[i] < array[i * 2 + 2]) {
+                k = i * 2 + 2
+            }
+            guard k != i else {
+                return
+            }
+            (array[i], array[k]) = (array[k], array[i])
+            i = k
         }
     }
     
--- LeetCode/347. Top K Frequent Elements/main.swift
+++ LeetCode/347. Top K Frequent Elements/main.swift
@@ -35,6 +35,10 @@ class SolutionSwift {
     }
 }
 
-let s = Solution()
-let k = s.topKFrequent([1,1,1,2,2,3,3,3,3,3], 2)
-print(k)
+//let s = Solution()
+//let heap = Heap.init([7,5,6,4,2,1], capacity: 10)
+//let heap = Heap.init([33,27,21,16,13,15,9,5,6,7,8,1,2], capacity: 20)
+//heap.insert(22)
+let heap = Heap.init([33,27,21,16,13,15,19,5,6,7,8,1,2,12], capacity: 20)
+heap.removeMax()
+print(heap.array)

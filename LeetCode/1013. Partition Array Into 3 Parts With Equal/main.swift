cbdf6733a55592616e05a49a52ebf2b56f7eeb5c
saidicaprio <hjw6160602@gmail.com>
2019-05-06 20:30:30 +0800

    add 1013. Partition Array Into 3 Parts With Equal Sum

---
+++ LeetCode/1013. Partition Array Into 3 Parts With Equal/main.swift
@@ -0,0 +1,24 @@
+//  https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum
+//  main.swift
+//  1013. Partition Array Into 3 Parts With Equal
+//
+//  Created by saidicaprio on 2019/5/6.
+//  Copyright © 2019 saidicaprio. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    func canThreePartsEqualSum(_ A: [Int]) -> Bool {
+        let count = A.count
+        for index1 in 0..<count-1 {
+            for index2 in 
+        }
+        return false
+    }
+}
+
+let s = Solution()
+print(s.canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1]))
+
+

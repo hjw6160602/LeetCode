e7d9ddb67ce15d14a94890cab1a355745e4ef942
hejiawei <hejiawei@qiyi.com>
2021-01-29 18:29:49 +0800

    优化写法

--- LeetCode/LeetCode.playground/Pages/88. Merge Sorted Array.xcplaygroundpage/Contents.swift
+++ LeetCode/LeetCode.playground/Pages/88. Merge Sorted Array.xcplaygroundpage/Contents.swift
@@ -13,19 +13,53 @@
 //输入：nums1 = [1], m = 1, nums2 = [], n = 0
 //输出：[1]
 
+//func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+//    var i = m - 1, j = n - 1
+//
+//    while i >= 0 || j >= 0 {
+//        if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {
+//            nums1[i + j + 1] = nums1[i]
+//            i -= 1
+//        } else {
+//            nums1[i + j + 1] = nums2[j]
+//            j -= 1
+//        }
+//    }
+//}
+
 func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-    var i = m - 1, j = n - 1
-    
-    while i >= 0 || j >= 0 {
+    var cur = m + n - 1
+    var i = m - 1
+    var j = n - 1
+    while j >= 0 || i >= 0 {
         if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[i + j + 1] = nums1[i]
+            nums1[cur] = nums1[i]
             i -= 1
+            cur -= 1
         } else {
-            nums1[i + j + 1] = nums2[j]
+            nums1[cur] = nums2[j]
             j -= 1
+            cur -= 1
+        }
+    }
+}
+
+func merge2(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    var cur = m + n - 1
+    var i = m - 1
+    var j = n - 1
+    while j >= 0  {
+        if i >= 0 && nums1[i] > nums2[j] {
+            nums1[cur] = nums1[i]
+            i -= 1
+            cur -= 1
+        } else {
+            nums1[cur] = nums2[j]
+            j -= 1
+            cur -= 1
         }
     }
 }
 
 var nums1 = [1,3,5,0,0,0]
-merge(&nums1, 3, [2,5,6], 3)
+merge2(&nums1, 3, [2,4,6], 3)

659af26657de28d50e256700673ba0b8cf87e4b4
hejiawei <hejiawei@qiyi.com>
2021-02-25 20:15:44 +0800

    复习两数之和

--- LeetCode/Array.Heap/01. 两数之和.swift
+++ LeetCode/Array.Heap/01. 两数之和.swift
@@ -4,10 +4,11 @@
 //
 //  Created by 江雪 on 2021/2/19.
 //  Copyright © 2021 saidicaprio. All rights reserved.
-//
+//  https://leetcode-cn.com/problems/two-sum
 
 import Foundation
 
+// 给定一个target
 extension Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dict: [Int:Int] = [:]
@@ -35,6 +36,40 @@ extension Solution {
         
         fatalError("No valid outputs")
     }
+    
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict[Int: Int] = [:]
+        for idx in 0..<nums.count {
+            if let x = dict[target - nums[idx]] {
+                return (nums[idx],idx, )
+            } else {
+                dict[nums[idx]] = idx
+            }
+        }
+    }
 }
 
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+//你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+//你可以按任意顺序返回答案。
+
+//示例 1：
+
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1]
+
+//示例 2：
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+
+//示例 3：
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+
+//提示：
+//2 <= nums.length <= 103
+//-109 <= nums[i] <= 109
+//-109 <= target <= 109
+//只会存在一个有效答案
 

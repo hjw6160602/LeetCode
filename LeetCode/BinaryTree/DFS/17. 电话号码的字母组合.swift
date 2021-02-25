0d7acb4bba75105e80dbbb8a100365d87aff227d
贺嘉炜 <hejiawei@hejiaweideMacBook-Pro.local>
2021-02-25 11:32:09 +0800

    add 电话号码的字母组合

--- LeetCode/BinaryTree/DFS/17. 电话号码的字母组合.swift
+++ LeetCode/BinaryTree/DFS/17. 电话号码的字母组合.swift
@@ -8,10 +8,58 @@
 
 import Foundation
 
-extension Solution {
+class LetterCombination {
+
+    var digits: String = ""
+    
+    let numberToStr = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+    
+    var combinations: [String] = []
+    
+    var combination: String = ""
+    
     func letterCombinations(_ digits: String) -> [String] {
-        return []
+        guard digits.count > 0 else {
+            return [String]()
+        }
+        self.digits = digits
+        dfs(0)
+        return combinations
     }
+    
+    // 深度优先搜索 正在搜索第idx层
+    private func dfs(_ index: Int) {
+        // 已经进入到最后一层了，不能再深入
+        if combination.count == digits.count {
+            // 得到了一个正确的解
+            combinations.append(combination)
+            return
+        }
+        
+        // 先枚举这一层可以做的所有选择
+        let currentStr = fetchCurrentStr(at: index)
+        
+        for char in currentStr {
+            combination.append(char)
+            dfs(index + 1)
+            combination.removeLast()
+        }
+        
+    }
+    private func fetchCurrentStr(at index: Int) -> String {
+        guard index >= 0 && index < digits.count else {
+            fatalError("Invalid index")
+        }
+        
+        let currentDigitChar = digits[digits.index(digits.startIndex, offsetBy: index)]
+        
+        guard let currentDigit = Int(String(currentDigitChar)), currentDigit >= 0, currentDigit < numberToStr.count else {
+            fatalError("Invalid digits")
+        }
+        
+        return numberToStr[currentDigit]
+    }
+    
 }
 
 //给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
---
+++ LeetCode/BinaryTree/DFS/46. 全排列.swift
@@ -0,0 +1,31 @@
+//
+//  46. 全排列.swift
+//  BinaryTree
+//
+//  Created by 贺嘉炜 on 2021/2/25.
+//  Copyright © 2021 saidicaprio. All rights reserved.
+//  https://leetcode-cn.com/problems/permutations
+
+import Foundation
+
+extension Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        return [[0]]
+    }
+}
+
+
+
+//给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+//示例:
+//输入: [1,2,3]
+//输出:
+//[
+//  [1,2,3],
+//  [1,3,2],
+//  [2,1,3],
+//  [2,3,1],
+//  [3,1,2],
+//  [3,2,1]
+//]

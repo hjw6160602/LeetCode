7af27e6548b837d01ac833d1466b2c19a6a6e708
hejiawei <hejiawei@qiyi.com>
2022-08-14 21:30:39 +0800

    add: 64. 最小路径和

--- LeetCode/DynamicPrograming/64. 最小路径和.swift
+++ LeetCode/DynamicPrograming/64. 最小路径和.swift
@@ -9,24 +9,77 @@
 import Foundation
 
 extension Solution {
+    
+//    44 ms 62.10%
+//    14.4 MB 19.35%
     func minPathSum(_ grid: [[Int]]) -> Int {
         let m = grid.count, n = grid[0].count
-        var dp = Array(repeating: Array.init(repeating: 0, count: m + 1),
-                       count: n + 1)
-        for i in 1...m {
-            for j in 1...n {
-                
-                dp[i][j] =  min(dp[i][j-1], dp[i-1][j]) + grid[i-1][j-1]
+        var dp = Array(repeating: Array(repeating: 0, count: n),
+                       count: m)
+        dp[0][0] = grid[0][0]
+        for j in 1..<n {
+            dp[0][j] = dp[0][j-1] + grid[0][j]
+        }
+        for i in 1..<m {
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+        }
+        for i in 1..<m {
+            for j in 1..<n {
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
             }
         }
-        return dp[m][n]
-        
+        return dp[m-1][n-1]
     }
+    
+//    52 ms 12.10%
+//    14 MB 91.13%
+    func minPathSumP1(_ grid: [[Int]]) -> Int {
+        let m = grid.count, n = grid[0].count
+        var dp = grid[0]
+        for j in 1..<n {
+            dp[j] += dp[j-1]
+        }
+        for i in 1..<m {
+            dp[0] += grid[i][0]
+            for j in 1..<n {
+                dp[j] = min(dp[j-1], dp[j]) + grid[i][j]
+            }
+        }
+        return dp[n-1]
+    }
+    
+//    44 ms 62.10%
+//    14.5 MB 12.09%
+    func minPathSumSoap(_ grid: [[Int]]) -> Int {
+        guard grid.count != 0 && grid[0].count != 0 else{
+            return 0
+        }
+        
+        let m = grid.count, n = grid[0].count
+        var dp = Array(repeating: Array(repeating: 0, count: n), count: m)
+        
+        for i in 0..<m {
+            for j in 0..<n {
+                if i == 0 && j == 0{
+                    dp[i][j] = grid[i][j]
+                } else if i == 0 {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                } else if j == 0 {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                } else {
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
+                }
+            }
+        }
+        return dp[m - 1][n - 1]
+    }
+    
 }
 
 func test64MinPathSum() {
-    let grid = [[1,3,1],[1,5,1],[4,2,1]]
-    let x = LeetCode.minPathSum(grid)
+//    let grid = [[1,3,1],[1,5,1],[4,2,1]]
+    let grid = [[1,2,3],[4,5,6]]
+    let x = LeetCode.minPathSumP1(grid)
     print(x)
 }
 //给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。

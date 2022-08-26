4033c5a21c6b9dc3c9655663bfe2ef83f52116e9
hejiawei <hejiawei@qiyi.com>
2022-08-26 16:35:11 +0800

    .prac: 240搜索二维矩阵

--- LeetCode/Search/240. 搜索二维矩阵 II.swift
+++ LeetCode/Search/240. 搜索二维矩阵 II.swift
@@ -10,53 +10,6 @@ import Foundation
 
 extension Solution {
     
-
-    
-    func searchMatrixP1(_ matrix: [[Int]], _ target: Int) -> Bool {
-        let m = matrix.count, n = matrix[0].count
-        
-        struct Corner {
-            var x: Int = 0
-            var y: Int = 0
-        }
-        var topLeft = Corner(x: 0, y: 0)
-        var btmRight = Corner(x: m-1, y: n-1)
-        
-        var i = m >> 1, j = n >> 1
-        
-        while i >= 0 && j >= 0 {
-            if matrix[i][j] == target {
-                return true
-            }
-            if matrix[i][j] > target {
-                if i == topLeft.x && j == topLeft.y {
-                    return false
-                }
-                // 缩小矩阵范围 先记录下上次的 i,j
-                btmRight = Corner(x: i, y: j)
-                i = (topLeft.x + i - 1) >> 1
-                j = (topLeft.y + j - 1) >> 1
-                if i == btmRight.x && j == btmRight.y {
-                    return false
-                }
-            } else {
-                if i == btmRight.x && j == btmRight.y {
-                    return false
-                }
-                // 扩大矩阵范围 先记录下上次的 i,j
-                topLeft = Corner(x: i, y: j)
-                
-                i = (btmRight.x + i + 1) >> 1
-                j = (btmRight.y + j + 1) >> 1
-                if i == topLeft.x && j == topLeft.y {
-                    return false
-                }
-            }
-            
-        }
-        return false
-    }
-    
     //    276 ms 15.28%
     //    14.7 MB 48.61%
     func searchMatrixSoap(_ matrix: [[Int]], _ target: Int) -> Bool {
@@ -100,27 +53,107 @@ extension Solution {
         }
         return false
     }
+    
+    //    256 ms 28.77%
+    //    14.4 MB 84.93%
+    // 根据左上角和右上角 来控制和缩小 矩阵范围 当 矩阵范围 缩小到4个值的时候 进入循环求解
+    func searchMatrixP1(_ matrix: [[Int]], _ target: Int) -> Bool {
+        let m = matrix.count, n = matrix[0].count
+        
+        typealias Corner = (x: Int, y: Int)
+        var topLeft = Corner(x: 0, y: 0)
+        var btmRight = Corner(x: m-1, y: n-1)
+        
+        var i = m >> 1, j = n >> 1
+        
+        while i >= 0 && j >= 0 {
+            if matrix[i][j] == target {
+                return true
+            }
+            if btmRight.x - topLeft.x < 2 && btmRight.y - topLeft.y < 2 {
+                return search4Nums(matrix, target)
+            }
+            
+            if matrix[i][j] > target {
+                // 缩小矩阵范围 先记录下上次的 i,j
+                let x = i - 1 >= 0 ? i : i - 1
+                let y = j - 1 >= 0 ? j : j - 1
+                btmRight = Corner(x: x, y: y)
+                i = (topLeft.x + i) >> 1
+                j = (topLeft.y + j) >> 1
+            } else {
+                // 扩大矩阵范围 先记录下上次的 i,j
+                let x = i + 1 >= m ? i : i + 1
+                let y = j + 1 >= n ? j : j + 1
+                topLeft = Corner(x: x, y: y)
+                
+                i = (btmRight.x + i) >> 1
+                j = (btmRight.y + j) >> 1
+            }
+        }
+        return false
+    }
+    
+    private func search4Nums(_ matrix: [[Int]], _ target: Int) -> Bool {
+        let m = matrix.count, n = matrix[0].count
+        
+        for i in 0..<m {
+            if matrix[i][0] > target {
+                return false
+            }
+            for j in 0..<n {
+                if matrix[i][j] == target {
+                    return true
+                }
+                if matrix[i][j] > target {
+                    break
+                }
+            }
+        }
+        return false
+    }
 }
 
 func test240searchMatrix() {
+    let matrix = [[1,4],[2,5]], target = 2
 //    let matrix = [[1,3,5]], target = 5
-//    let matrix = [[1, 4, 7, 11,15],
-//                  [2, 5, 8, 12,19],
-//                  [3, 6, 9, 16,22],
-//                  [10,13,14,17,24],
-//                  [18,21,23,26,30]]
-//    let target = 5
-    
+//    let (matrix, target) = testCase3()
+    let x = LeetCode.searchMatrixP1(matrix, target)
+    print(x)
+}
+
+fileprivate func testCase1() -> (matrix: [[Int]], target: Int) {
+    let matrix = [[1, 4, 7, 11,15],
+                  [2, 5, 8, 12,19],
+                  [3, 6, 9, 16,22],
+                  [10,13,14,17,24],
+                  [18,21,23,26,30]]
+    let target = 5
+    return (matrix, target)
+}
+
+fileprivate func testCase2() -> (matrix: [[Int]], target: Int) {
     let matrix = [[1, 4, 7, 11,15],
                   [2, 5, 8, 12,19],
                   [3, 6, 9, 16,22],
                   [10,13,14,17,24],
                   [18,21,23,26,30]]
     let target = 20
-    
-    let x = LeetCode.searchMatrixP1(matrix, target)
-    print(x)
+    return (matrix, target)
 }
+
+fileprivate func testCase3() -> (matrix: [[Int]], target: Int) {
+    let matrix = [[3,6,9,12,17,22],
+                  [5,11,11,16,22,26],
+                  [10,11,14,16,24,31],
+                  [10,15,17,17,29,31],
+                  [14,17,20,23,34,37],
+                  [19,21,22,28,37,40],
+                  [22,22,24,32,37,43]]
+     let target = 20
+    return (matrix, target)
+}
+
 
 //编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
 
